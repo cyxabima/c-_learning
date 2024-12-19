487a3df7d814b7910325a17ea3d2584df4798de7
cyxabima <ukashaanwerali@gmail.com>
2024-12-19 12:02:27 +0500

    maza

---
+++ chai/02/02_datatype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks = 122;
+    char grade = 'A';
+    float GPA = 4.0; 
+    long double fee = 1.2244; // 5 pecision point 
+
+    short std = 12000;
+    long  uni_revenue = 2000000000000000000;
+    unsigned attendence = -12;
+
+    cout <<uni_revenue << endl ;
+    return 0;
+}
